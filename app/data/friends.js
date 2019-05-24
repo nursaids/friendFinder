9a0674b7ae853d2d87a16a85e1c0a2eca4b0073a
Nursaid Saidmakhmudov <uzbltd@Nursaids-MacBook-Pro.local>
2019-05-23 17:06:52 -0700

    friends.js added frinds list

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+
+
+const friends = [
+
+    {
+        name: "Stewie Griffin",
+        photo: "https://images.app.goo.gl/YBVBxYwBC8dJxYMLA",
+        scores:[ 5, 4, 3, 2, 1, 2, 3, 4, 5, 4]
+    },
+
+    {
+        name: "Brian Griffin",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8t5YuvRe4cX249aCWjfvgptkTrmTrKnyuEm9M7_hTXGrEZKtf",
+        scores:[ 5, 4, 3, 2, 1, 2, 3, 4, 5, 4]
+    },
+
+	
+	{
+		name : "Meg Griffin",
+		photo : " ",
+		scores : [ 3, 4, 5, 1, 2, 3, 4, 5, 1, 2 ]
+	},
+	{
+		name : "Quagmire",
+		photo : " ",
+		scores : [ 4, 5, 1, 2, 3, 4, 5, 1, 2, 3 ]
+	},
+	{
+		name : "Joe",
+		photo : " ",
+		scores : [ 5, 1, 2, 3, 4, 5, 1, 2, 3, 4 ]
+	},
+	{
+		name : "Louis Griffin",
+		photo : " ",
+		scores : [ 3, 2, 3, 2, 3, 2, 3, 2, 3, 2 ]
+	},
+	{
+		name : "Dr. Hartman",
+		photo : " ",
+		scores : [ 3, 2, 3, 4, 3, 2, 3, 4, 3, 2 ]
+	},
+	{
+		name : "Chris Griffin",
+		photo : " ",
+		scores : [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+	},
+	{
+		name : "",
+		photo : "Cleveland Brown",
+		scores : [ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
+	}
+]
+module.exports = friends;
+
