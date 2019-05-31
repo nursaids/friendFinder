f16248c44f0902e149daad69eaaaa910fde3559d
Nursaid Saidmakhmudov <uzbltd@Nursaids-MacBook-Pro.local>
2019-05-30 18:22:15 -0700

    init

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,50 +4,50 @@ var friends = [
 
     {
         "name": "Jeremy Clarkson",
-        "photo": "https://images.app.goo.gl/YBVBxYwBC8dJxYMLA",
+        "photo": "https://via.placeholder.com/150",
         "scores":[ 5, 4, 3, 2, 1, 2, 3, 4, 5, 4]
     },
 
     {
         "name": "Stig",
-        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8t5YuvRe4cX249aCWjfvgptkTrmTrKnyuEm9M7_hTXGrEZKtf",
+        "photo": "https://via.placeholder.com/150",
         "scores":[ 5, 4, 3, 2, 1, 2, 3, 4, 5, 4]
     },
 
 	
 	{
 		"name" : "James May",
-		"photo" : " ",
+		"photo" : " https://via.placeholder.com/150",
 		"scores" : [ 3, 4, 5, 1, 2, 3, 4, 5, 1, 2 ]
 	},
 	{
 		"name" : "Richard Hammond",
-		"photo" : " ",
+		"photo" : " https://via.placeholder.com/150",
 		"scores" : [ 4, 5, 1, 2, 3, 4, 5, 1, 2, 3 ]
 	},
 	{
 		"name" : "Mr Slow",
-		"photo" : " ",
+		"photo" : " https://via.placeholder.com/150",
 		"scores" : [ 5, 1, 2, 3, 4, 5, 1, 2, 3, 4 ]
 	},
 	{
 		"name" : "Mr Bin",
-		"photo" : " ",
+		"photo" : " https://via.placeholder.com/150",
 		"scores" : [ 3, 2, 3, 2, 3, 2, 3, 2, 3, 2 ]
 	},
 	{
 		"name" : "Michael Schumacher",
-		"photo" : " ",
+		"photo" : " https://via.placeholder.com/150",
 		"scores" : [ 3, 2, 3, 4, 3, 2, 3, 4, 3, 2 ]
 	},
 	{
 		"name" : "Lewis Hamilton",
-		"photo" : " ",
+		"photo" : "https://via.placeholder.com/150 ",
 		"scores" : [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
 	},
 	{
 		"name" : "Michael Jackson",
-		"photo" : "",
+		"photo" : "https://via.placeholder.com/150",
 		"scores" : [ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
 	}
 ]
